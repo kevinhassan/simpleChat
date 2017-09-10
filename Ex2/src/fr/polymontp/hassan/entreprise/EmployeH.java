212bc0f090a5af3e837e23c7cff99d56991b0f9b
kevin.hassan <kevin.hassan@etu.umontpellier.fr>
2017-09-10 18:34:48 +0200

    Add exercice 2

---
+++ Ex2/src/fr/polymontp/hassan/entreprise/EmployeH.java
@@ -0,0 +1,29 @@
+package fr.polymontp.hassan.entreprise;
+
+public class EmployeH extends Employe{
+	private double nbHeure;
+	private double tauxH;
+	private double pourcentage;
+	
+	public EmployeH(String nom) {
+		super(nom);
+	}
+	
+	public EmployeH(String nom,double nbHeure,double tauxH,double pourcentage) {
+		super(nom);
+		this.nbHeure = nbHeure;
+		this.tauxH = tauxH;
+		this.pourcentage = pourcentage;
+	}
+	
+	public double getSalaire(){
+		return (this.tauxH*(1+this.pourcentage/100)*this.nbHeure+this.tauxH*35);
+	}
+	
+	public void setInfoSalaire(double nbHeure,double tauxH,double pourcentage){
+		this.nbHeure = nbHeure;
+		this.tauxH = tauxH;
+		this.pourcentage = pourcentage;
+		
+	}
+}
